81c0db322b9a025fa00a351707ddc7c0e093e5f2
mur3l <tippitauno@gmail.com>
2025-09-08 15:23:57 +0300

    CalcPage_Model COMMIT

---
+++ ScientificCalculator/Views/CalculatorPageViewModel.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ScientificCalculator.ViewModels
+{
+    [INotifyPropertyChanged]
+
+    internal partial class CalculatorPageViewModel : ObservableObject
+
+    {
+        [ObservableProperty]
+        private string inputText = string.Empty;
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
+
+        [RelayCommand]
+
+        private void Reset()
+        {
+            calculatedResult = "0";
+            InputText = string.Empty;
+            isSciOpWaiting = false;
+        }
+
+        [RelayCommand]
+        private void Calculate()
+        {
+            if (InputText.Lenght == 0)
+            {
+                return;
+            }
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+            try
+            {
+                var inputString = NormalizeInputString();
+                var expression = new NCalc.Expression(inputString);
+                var result = expression.Evaluate();
+
+                CalculatedResult = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                CalculatedResult = "NaN";
+            }
+        }
+
+        private string NormalizeInputString()
+        {
+            Dictionary<string, string> _opMapper = new()
+            {
+                {"×", "*"},
+                {"÷", "/"},
+                {"SIN", "Sin"},
+                {"COS","Cos"},
+                {"TAN", "Tan"},
+                {"ASIN", "Asin"},
+                {"ACOS", "Acos"},
+                {"ATAN", "Atan"},
+                {"LOG", "Log"},
+                {"EXP", "Exp"},
+                {"LOG10", "Log10"},
+                {"POW", "Pow"},
+                {"SQRT", "Sqrt"},
+                {"ABS", "Abs"},
+
+            };
+
+            var retString = InputText;
+
+            foreach (var key in _opMapper.Keys)
+            {
+                retString = retString.Replace(key, _opMapper[key]);
+            }
+
+            return retString;
+        }
+
+        [RelayCommand]
+        private void Backspace()
+        {
+            if (InputText.Lenght > 0)
+            {
+                InputText = InputText.Substring(0, InputText.Length - 1);
+            }
+        }
+
+        [RelayCommand]
+        private void NumberInput(string key)
+        {
+            InputText += key;
+        }
+
+        [RelayCommand]
+        private void MathOperator(string op)
+        {
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+            InputText += $" {op}";
+        }
+
+        [RelayCommand]
+        private void RegionOperator(string op)
+        {
+            if (isSciOpWaiting)
+            {
+                InputText += ")";
+                isSciOpWaiting = false;
+            }
+            InputText += $" {op}";
+        }
+
+        [RelayCommand]
+        private void ScientificOperator(string op)
+        {
+                InputText += $"{op}(";
+                isSciOpWaiting = false;
+        }
+
+    }
+
+}
