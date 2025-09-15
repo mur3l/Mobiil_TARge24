39a1b9d628df7cdbd996a1f9325088fb43f256a0
mur3l <tippitauno@gmail.com>
2025-09-15 15:25:13 +0300

    Maui FONT Change_COMMIT

--- MauiPlanets/MauiProgram.cs
+++ MauiPlanets/MauiProgram.cs
@@ -11,8 +11,9 @@ namespace MauiPlanets
                 .UseMauiApp<App>()
                 .ConfigureFonts(fonts =>
                 {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Montserrat-Medium.ttf", "RegularFont");
+                    fonts.AddFont("Montserrat-SemiBold.ttf", "MediumFont");
+                    fonts.AddFont("Montserrat-Bold.ttf", "BoldFont");
                 });
 
 #if DEBUG
