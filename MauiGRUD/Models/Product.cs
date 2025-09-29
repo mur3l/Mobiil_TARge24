71e14ac72b68ce6301caa76505b416825c4d51e1
mur3l <tippitauno@gmail.com>
2025-09-29 15:00:44 +0300

    Models folder and Product class_COMMIT

---
+++ MauiGRUD/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiGRUD.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Product Clone() => MemberwiseClone() as Product;
+        public (bool IsValid, string? ErrorMessage) Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                return (false, $"{nameof(Name)} is required");
+            }
+            else if (Price <= 0)
+            {
+                return (false, $"{nameof(Price)} should be greater than 0");
+            }
+            return (true, null);
+        }
+
+    }
+}
